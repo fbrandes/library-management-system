b2f2139ebc7b40d71bda61878788b3d2cb9fd7cd
Fabian Brandes <15704574+fbrandes@users.noreply.github.com>
2023-02-24 16:03:32 +0100

    fix openlibrary service not starting

--- bookinfo/openlibrary-service/src/main/java/com/github/fbrandes/library/openlibrary/OpenLibraryServiceApplication.java
+++ bookinfo/openlibrary-service/src/main/java/com/github/fbrandes/library/openlibrary/OpenLibraryServiceApplication.java
@@ -6,6 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class OpenLibraryServiceApplication {
     public static void main(String[] args) {
-        SpringApplication.run(SpringBootApplication.class, args);
+        SpringApplication.run(OpenLibraryServiceApplication.class, args);
     }
 }
