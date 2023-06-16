9edf699d9f8733a1e88054f86de68cddf82d9e7c
Fabian Brandes <fbrandes@noreply.com>
2023-06-16 13:58:55 +0200

    created basic gateway impl and test

--- gateway/src/test/java/com/github/fbrandes/library/gateway/GatewayApplicationTests.java
+++ gateway/src/test/java/com/github/fbrandes/library/gateway/GatewayApplicationTests.java
@@ -1,13 +1,32 @@
 package com.github.fbrandes.library.gateway;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
+@ActiveProfiles("dev")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class GatewayApplicationTests {
+	@Autowired
+	private WebTestClient client;
 
-	@Test
-	void contextLoads() {
+	@BeforeEach
+	void setup(ApplicationContext context) {
+		client = WebTestClient.bindToApplicationContext(context).build();
+	}
+	@ParameterizedTest
+	@ValueSource(strings = {"info", "health", "gateway/routes"})
+	void actuatorEndpointsAvailable(String endpoint) {
+		client.get()
+			.uri("/actuator/" + endpoint)
+			.exchange()
+			.expectStatus()
+			.isOk();
 	}
 
 }
---
+++ gateway/src/test/java/com/github/fbrandes/library/gateway/GatewayIT.java
@@ -0,0 +1,7 @@
+package com.github.fbrandes.library.gateway;
+
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@Testcontainers
+public class GatewayIT {
+}
